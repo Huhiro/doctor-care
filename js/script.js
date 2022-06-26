352daaa56ebf598c4347d7fdec3ab0ab6131ed61
Huhiro <huhiroo@gmail.com>
2022-06-26 13:02:10 -0300

    att toggle

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
 window.onscroll = function() {myFunction()};
 
-const navbar = document.getElementById("navbar");
-const doctor = document.getElementById("doctor");
-const care = document.getElementById("care");
+const toggle = document.getElementById("toggle")
+const navbar = document.getElementById("navbar")
+const doctor = document.getElementById("doctor")
+const care = document.getElementById("care")
 const menuOne = document.getElementById("menu-one")
 const menuTwo = document.getElementById("menu-two")
 const menuThree = document.getElementById("menu-three")
 const menuFour = document.getElementById("menu-four")
 const primaryButton = document.getElementById("primary-button")
 
+
 const sticky = navbar.offsetTop;
+
+function toggleNav() {
+  const x = document.getElementById("open-toggle");
+  const y = document.getElementById("logo")
+
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+    // x.style.marginLeft = "40px";
+    y.style.flexDirection = "column";
+    y.style.display = "flex";
+    y.style.lineHeight = "20px";
+    y.style.textAlign = "center";
+  } else {
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
+    toggle.classList.add("icon-fixed")
     navbar.classList.add("sticky");
     navbar.classList.add("navigation-fixed");
     doctor.classList.add("doctor-fixed");
@@ -25,6 +45,8 @@ function myFunction() {
     
     }
     else {
+     
+      toggle.classList.remove("icon-fixed")
       navbar.classList.remove("sticky");
       navbar.classList.remove("navigation-fixed")
       doctor.classList.remove("doctor-fixed");
